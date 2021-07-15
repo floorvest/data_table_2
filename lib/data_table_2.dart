32e27b1d5af44a9e6d37e8d1f57dcf1a49877a10
Patranto Bagus Prabowo <patranto.prabowo@gmail.com>
2021-07-15 13:56:37 +0700

    fixing

--- lib/data_table_2.dart
+++ lib/data_table_2.dart
@@ -618,7 +618,7 @@ class DataTable2 extends DataTable {
               ? effectiveHorizontalMargin / 2
               : effectiveHorizontalMargin);
 
-      var columnWidth = totalColWidth;
+      var columnWidth = totalColWidth / columns.length;
       // var totalWidth = 0.0;
       bool rowsExist = false;
       int length = columns.length;
